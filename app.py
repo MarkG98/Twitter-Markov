f87782980c79bcaff3f12d97130f22877418835a
Mark Goldwater <mark.goldwater@students.olin.edu>
2018-05-16 13:56:48 -0400

    Displays markov to html and navbar navigates

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from markov import Markov
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+    if request.method == 'POST':
+        username = request.form['username']
+        Mgen = Markov()
+        res = Mgen.markov(username)
+
+        return render_template('markovres.html', res=res)
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
